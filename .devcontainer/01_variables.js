dde4bf91717f8d92c19e4310d1b0df6c7111fa02
Vrushank30 <168173515+Vrushank30@users.noreply.github.com>
2025-08-31 08:49:39 +0000

    variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14453
+let accountName = "Vrushank"
+var accountEmail = "vrushankfcb@gmail.com"
+let accountPassword = "cdm@1234"
+accountcity = "tumkur"
+
+accountEmail = "vr@gmail.com"
+accountcity = "Bengaluru"
+
+console.table([accountEmail,accountId,accountName,accountPassword,accountcity])
