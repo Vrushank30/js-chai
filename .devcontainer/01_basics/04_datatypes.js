a235248383f3e2f259cf6a638c160ec18ca99880
Vrushank30 <168173515+Vrushank30@users.noreply.github.com>
2025-09-06 13:02:23 +0000

    ...

---
+++ .devcontainer/01_basics/04_datatypes.js
@@ -0,0 +1,15 @@
+const id = Symbol('1234');
+const idTwo = Symbol('1234');
+// console.log(id == idTwo);
+
+const players = ["messi","neymar","suarez","lewandowski"];
+let myObj = {
+    name : "Vrushank",
+    age : 20,
+};
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+myFunction();
+console.log(typeof players);
