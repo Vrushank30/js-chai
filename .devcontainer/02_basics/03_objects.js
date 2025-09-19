15ca02432e15eaab0aee77249b292d829e110b33
Vrushank30 <168173515+Vrushank30@users.noreply.github.com>
2025-09-19 15:43:50 +0000

    ...

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -16,8 +16,19 @@ const JsUser = {
     isLoggedIn : false,
     lastLoggedInDays : ["Monday","Tuesday"]
 }
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full Name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full Name"]);
+// console.log(typeof (mySym));
+
+JsUser.email = "vrushank@city.com"
+//Object.freeze(JsUser);
+JsUser.email = "vrushank@npm.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello");
+}
+console.log(JsUser.greeting());
 
 
